47ce56c93e3196e574efffba6d786adef4892273
Ifeanyi Ofikulu <ifeanyiofi@gmail.com>
2023-06-04 15:15:25 +0100

    commit message

--- src/components/Nftfooter.js
+++ src/components/Nftfooter.js
@@ -2,10 +2,10 @@ import avatar from '../images/image-avatar.png';
 
 const Nftfooter = () => {
     return (
-        <p className='footer'>
+        <div className='footer'>
           <img className='avatar' src={avatar} alt='avatar icon'/>
          <p className='footer_text'> Creation of<span className='link'> Jules Wyvern</span> </p>
-        </p>
+        </div>
     );
 }
 
